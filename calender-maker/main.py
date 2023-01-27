7b17b30e707baac5b12486f62b817f2b4e7520eb
Samkanja <samuelkanja4@gmail.com>
2023-01-27 23:37:13 +0300

    calender maker

---
+++ calender-maker/main.py
@@ -0,0 +1,4 @@
+import datetime
+
+DAYS = ('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday')
+MONTHS = ('January','February','March','April','May','June','July','August','September','October','November','December')
